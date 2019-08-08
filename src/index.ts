48a33d29b68f4aa9ff23c76dc1eebbd785390a62
Timur Asayonok <t.asaynok@gmail.com>
2019-08-08 19:40:33 +0200

    Added react-native-navigation lib for application navigation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-import { AppRegistry } from 'react-native'
+import { Navigation } from "react-native-navigation";
 import App from './Root';
 
-AppRegistry.registerComponent('shoppingListApp', () => App);
+// AppRegistry.registerComponent('shoppingListApp', () => App);
+
+Navigation.registerComponent('shoppingListApp', () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+     component: {
+        name: "shoppingListApp"
+      }
+   }
+  });
+});
